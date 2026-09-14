0d384b7360bb9239e1162a546fde2362f190fe4f
Andrea <aikhor0907@gmail.com>
2026-09-14 17:36:17 -0500

    feat(ui): configure again PrimerVue

--- src/main.js
+++ src/main.js
@@ -4,23 +4,25 @@ import App from './app.vue'
 import i18n from "./i18n.js";
 import PrimeVue from 'primevue/config';
 import Material from '@primeuix/themes/material';
-import '@primeicons/primeincons.css';
-import 'primerflex/primerflex.css';
-import {Button, ButtonGroup, Menu, Popover, SelectButton, Toolbar, Tooltip} from "primevue";
+import '@primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import {Button, Menu, SelectButton, Drawer, Avatar, Card, Toolbar, Tooltip, Menubar, Popover} from "primevue";
 
+const primeUiLicenseKey= import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
 
 createApp(App)
     .use(i18n)
-    .use(PrimeVue,  {ripple: true, theme: {preset:Material}, license:primeUiLicenseKey})
-    .component( 'pv-button', Button)
-    .component( 'pv-select-button', SelectButton )
-    .component( 'pv-avatar', Avatar )
-    .component( 'pv-drawer', Drawer )
-    .component( 'pv-card', Card )
-    .component( 'pv-toolbar', Toolbar )
-    .component( 'pv-menu', Menu )
-    .component( 'pv-menu-bar', Menubar)
-    .component ( 'pv-popover', Popover )
-    .directive ( 'tooltip', Tooltip )
+    .use(PrimeVue, { ripple: true, theme: { present: Material }, license: primeUiLicenseKey})
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-avatar', Avatar)
+    .component('pv-drawer', Drawer)
+    .component('pv-menu', Menu)
+    .component('pv-card', Card)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-menubar', Menubar)
+    .component('pv-popover', Popover)
+    .directive('tooltip', Tooltip)
+    .mount('#app')
 
     .mount('#app')
