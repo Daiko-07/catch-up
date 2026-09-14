3cd2a9f336de9eaf36e154cc618f868f319cb315
Andrea <aikhor0907@gmail.com>
2026-09-14 17:35:14 -0500

    feat(ui): configure PrimerVue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
+import i18n from "./i18n.js";
+import PrimeVue from 'primevue/config';
+import Material from '@primeuix/themes/material';
+import '@primeicons/primeincons.css';
+import 'primerflex/primerflex.css';
+import {Button, ButtonGroup, Menu, Popover, SelectButton, Toolbar, Tooltip} from "primevue";
+
 
 createApp(App)
-    .use(i18n())
+    .use(i18n)
+    .use(PrimeVue,  {ripple: true, theme: {preset:Material}, license:primeUiLicenseKey})
+    .component( 'pv-button', Button)
+    .component( 'pv-select-button', SelectButton )
+    .component( 'pv-avatar', Avatar )
+    .component( 'pv-drawer', Drawer )
+    .component( 'pv-card', Card )
+    .component( 'pv-toolbar', Toolbar )
+    .component( 'pv-menu', Menu )
+    .component( 'pv-menu-bar', Menubar)
+    .component ( 'pv-popover', Popover )
+    .directive ( 'tooltip', Tooltip )
+
     .mount('#app')
